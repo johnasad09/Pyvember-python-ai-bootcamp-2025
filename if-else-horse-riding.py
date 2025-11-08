534df3ab8c5b7222be0c3c2a55e7b1f6d3397b18
Asad Ullah <johnasad09@gmail.com>
2025-11-08 21:02:22 +0500

    Create if-else-horse-riding.py

---
+++ if-else-horse-riding.py
@@ -0,0 +1,10 @@
+# this python program checks the age and height for horse riding using if-else/conditional statements
+age = float(input("Enter your age"))
+height = float(input("Enter your height"))
+if age >= 15:
+    if height >=4:
+        print("You are allowed for horse riding")
+    else:
+        print("Sorry, You are not allowed for horse riding.")
+else:
+    print("Sorry, You are not allowed for horse riding.")
